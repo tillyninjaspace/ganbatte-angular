6b80367eea7ea19511367f4b499d625313c99977
tillyninjaspace <squealingpet@gmail.com>
2021-04-06 14:41:43 -0700

    delete button for Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,8 +28,20 @@ export class AppComponent {
     isLiked: true
   }
 
-  courses = [ ];
+  courses = [
+    {id: 1,name: "course1"},
+    {id: 2,name: "course2"},
+    {id: 3,name: "course3"},
+   ];
 
+   onAdd() {
+     this.courses.push({id: 4, name: 'course4'});
+   }
+
+   onRemove(course: any) {
+     let index = this.courses.indexOf(course);
+     this.courses.splice(index, 1)
+   }
   viewMode = 'map';
 
 }
